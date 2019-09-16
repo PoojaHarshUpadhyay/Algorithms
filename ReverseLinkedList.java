47d424b21fc3c763108267e1a018cd33ef20b751
Pooja Upadhyay <upooja2290@gmail.com>
2019-09-15 20:58:05 -0500

    Reverse a linked list

---
+++ ReverseLinkedList.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode next = null;
+        if(head == null ) {
+            return null;
+        }
+        while(current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+}
