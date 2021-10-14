23c5767189fed1e7d86ed6ab61f6ad97e4df7832
Pooja Upadhyay <upooja2290@gmail.com>
2021-10-14 15:08:08 -0500

    Update QuickSortAlgo.java

--- QuickSortAlgo.java
+++ QuickSortAlgo.java
@@ -1,55 +1,48 @@
-import java.lang.reflect.Array;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
-public class Main {
+public class Hello {
+    /**
+     * key, value
+     *
+     * @param args
+     */
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        Hello obj = new Hello();
+        int[] val = {8, 5, 4, 3, 2};
+        int[] res = obj.quickSort(val, 0, val.length-1);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
 
-
-    int[] A = {7, 2, 1, 6, 8, 5, 3, 4};
-    int start = 0;
-    int end = A.length - 1;
-    Main main = new Main();
-    main.QuickSort(A, start, end);
-    for (int i = 0; i < A.length; i++) {
-      System.out.println(A[i]);
-    }
-  }
-
-  private void QuickSort(int[] A, int start, int end) {
-    if (start >= end) {
-      return;
-    }
-    int pivot = partition(A, start, end);
-    QuickSort(A, start, pivot - 1);
-    QuickSort(A, pivot + 1, end);
-  }
-
-
-  private int partition(int[] A, int start, int end) {
-    //i changes if the current value is less than pivot.
-    int i = 0;
-    int pivot = A[end];
-
-
-    for (int c = 0; c < A.length; c++) {
-      if (A[c] < pivot) {
-        //swap c and i if the current index value is less than pivot index value and increment i
-        int temp = A[c];
-        A[c] = A[i];
-        A[i] = temp;
-        i++;
-      }
-    }
-    if (A[end] < A[i]) {
-      int temp = A[end];
-      A[end] = A[i];
-      A[i] = temp;
     }
 
-    return i;
-  }
+    private int[] quickSort(int[] A, int start, int end) {
+        if (start < end) {
+            int partition = partition(A, start, end);
+            quickSort(A, start, partition - 1);
+            quickSort(A, partition + 1, end);
+        }
+
+        return A;
+    }
+
+    private int partition(int[] A, int start, int end) {
+        int pIndex = start;
+        for (int i = start; i < end; i++) {
+            if (A[i] < A[end]) {
+                //swap i and pIndex if the current index value is less than pivot index value and increment pIndex
+                int temp = A[i];
+                A[i] = A[pIndex];
+                A[pIndex] = temp;
+                pIndex++;
+            }
+        }
+        //swap
+        int temp = A[end];
+        A[end] = A[pIndex];
+        A[pIndex] = temp;
+
+        return pIndex;
+    }
 }
-
