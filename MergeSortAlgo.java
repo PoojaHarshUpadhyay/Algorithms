e1b185fffa18ef634d9eb61569c0aeb4323d229b
Pooja Upadhyay <upooja2290@gmail.com>
2021-10-14 16:58:15 -0500

    Update MergeSortAlgo.java

--- MergeSortAlgo.java
+++ MergeSortAlgo.java
@@ -1,66 +1,73 @@
-import java.lang.reflect.Array;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
-public class Main {
+public class Hello {
+    /**
+     * key, value
+     *
+     * @param args
+     */
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        Hello obj = new Hello();
+        int[] val = {2, 4, 1, 6, 8, 5, 3, 7};
+        int[] res = obj.mergeSort(val);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
 
-    int[] Arry = {14, 15, 11, 2};
-    mergeSort(Arry, 0, Arry.length);
-    for (int i = 0; i < Arry.length; i++) {
-      System.out.println(Arry[i]);
     }
-  }
 
-  static void mergeSort(int[] arr, int m, int n) {
-    int start = m;
-    int end = n;
-    int mid = arr.length / 2;
-    if (mid == 0) {
-      return;
-    }
-    int[] lArr = new int[mid];
-    int[] rArr = new int[mid];
-    int k = 0;
-    for (int i = 0; i < lArr.length; i++) {
-      lArr[i] = arr[i];
-      k++;
-    }
-    for (int j = 0; j < rArr.length; j++) {
-      rArr[j] = arr[k];
-      k++;
-    }
-    mergeSort(lArr, start, mid);
-    mergeSort(rArr, mid + 1, end);
-    merge(lArr, rArr, arr);
-  }
+    private int[] mergeSort(int[] A) {
+        int n = A.length;
+        int mid = A.length / 2;
 
-  static void merge(int[] lArr, int[] rArr, int[] Arr) {
+        int[] left = new int[mid];
+        int[] right = new int[n - mid];
+        if (mid != 0) {
 
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    while (i < lArr.length && j < rArr.length) {
-      if (lArr[i] > rArr[j]) {
-        Arr[k] = rArr[j];
-        j++;
-      } else {
-        Arr[k] = lArr[i];
-        i++;
-      }
-      k++;
+            for (int i = 0; i < mid; i++) {
+                left[i] = A[i];
+            }
+            int p = 0;
+            for (int j = mid; j < n; j++) {
+                right[p] = A[j];
+                p++;
+            }
+            mergeSort(left);
+            mergeSort(right);
+            merge(left, right, A);
+
+        }
+        return A;
     }
-    while (i < lArr.length) {
-      Arr[k] = lArr[i];
-      i++;
-      k++;
+
+
+    private void merge(int[] left, int[] right, int[] A) {
+        int nL = left.length;
+        int nR = right.length;
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < nL && j < nR) {
+            if (left[i] < right[j]) {
+                A[k] = left[i];
+                i++;
+            } else {
+                A[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < nL) {
+            A[k] = left[i];
+            k++;
+            i++;
+        }
+        while (j < nR) {
+            A[k] = right[j];
+            k++;
+            j++;
+        }
     }
-    while (j < rArr.length) {
-      Arr[k] = rArr[j];
-      j++;
-      k++;
-    }
-  }
 }
